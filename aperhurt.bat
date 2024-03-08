02d301849e54d74da369eed7ea18577038a93c12
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-03-08 14:18:13 +0300

    Add files via upload

---
+++ aperhurt.bat
@@ -0,0 +1,185 @@
+echo off
+color 6
+mode con:cols=85 lines=40
+cls
+
+echo  ................................................................................
+echo  .............................x+x@@@@@@@@@@@@@@@@#x..............................
+echo  .............................%@@@@@@@@@@@@@@@@@@@@+...x.........................
+echo  .....................-@@x......+@@@@@@@@@@@@@@@@@@@...x@@@......................
+echo  ...................x@@@@@@@x......%@@@@@@@@@@@@@@@@....@@@@@x...................
+echo  .................@@@@@@@@@@@@x......-@@@@@@@@@@@@@@x...@@@@@@@@.................
+echo  ...............x@@@@@@@@@@@@@@@@+......x@@@@@@@@@@@@...x@@@@@@@@x...............
+echo  .............#@@@@@@@@@@@@@@@@@@@@@x......#@@@@@@@@@....@@@@@@@@@@x.............
+echo  ............@@@@@@@@@@@@@@@@@@@@@@@@@........%@@@@@@x...x@@@@@@@@@@@............
+echo  ..........x@@@@@@@@@@@@@@@@@@@@@@@x............+@@@@@...+@@@@@@@@@@@@x..........
+echo  .........x@@@@@@@@@@@@@@x.........................x@@:...@@@@@@@@@@@@@x.........
+echo  ........x@@@@@@@.....................................x...x@@@@@@@@@@@@..........
+echo  .........................................................:@@@@@@@@@@+...........
+echo  ........@@@@@@@@@@@@@@@@..................................@@@@@@@@@-...:@.......
+echo  .......+@@@@@@@@@@@@@@@...................................x@@@@@@@....+@@x......
+echo  ......@@@@@@@@@@@@@@@x....................................-@@@@@x....@@@@@......
+echo  .....x@@@@@@@@@@@@@@+......................................@@@@x....@@@@@@#.....
+echo  .....@@@@@@@@@@@@@@-.......................................#@@x....@@@@@@@@.....
+echo  .....@@@@@@@@@@@@@.........................................:@....+@@@@@@@@@.....
+echo  .....@@@@@@@@@@@x...............................................#@@@@@@@@@@.....
+echo  .....@@@@@@@@@@#...............................................%@@@@@@@@@@@.....
+echo  .....@@@@@@@@@x....@x.........................................@@@@@@@@@@@@@.....
+echo  .....@@@@@@@@:...x@@@.......................................x@@@@@@@@@@@@@@.....
+echo  .....@@@@@@@....#@@@@:.....................................x@@@@@@@@@@@@@@#.....
+echo  ......@@@@@....%@@@@@@....................................@@@@@@@@@@@@@@@@......
+echo  ......x@@x....@@@@@@@@...................................@@@@@@@@@@@@@@@#x......
+echo  .......@x....@@@@@@@@@.................................:@@@@@xxx-...............
+echo  ...........-@@@@@@@@@@#...............................:xx.......................
+echo  ..........x@@@@@@@@@@@@...+-...................................+@@@@@@@x........
+echo  .........+@@@@@@@@@@@@@x...@@x.......................xxx@@@@@@@@@@@@@@xx........
+echo  ..........#@@@@@@@@@@@@#...x@@@@x...........x#x@@@@@@@@@@@@@@@@@@@@@@#..........
+echo  ...........:@@@@@@@@@@@@...-@@@@@@@x.....x@@@@@@@@@@@@@@@@@@@@@@@@@@x...........
+echo  .............#@@@@@@@@@@:...@@@@@@@@@%-.....-@@@@@@@@@@@@@@@@@@@@@#.............
+echo  ...............@@@@@@@@@x...@@@@@@@@@@@@#......x@@@@@@@@@@@@@@@@@...............
+echo  ................:@@@@@@@@...+@@@@@@@@@@@@@@+......%@@@@@@@@@@@@.................
+echo  ...................@@@@@@:...@@@@@@@@@@@@@@@@@:.....+@@@@@@@@...................
+echo  ......................x@@x...x@@@@@@@@@@@@@@@@@@#......+@@......................
+echo  .........................x...-@@@@@@@@@@@@@@@@@@@@@x............................
+echo  ..............................xx@@@@@@@@@@@@@@@@xxxxx...........................
+echo  ................................................................................ 
+echo This was a triumph.
+timeout /t 1 > nul
+echo  I'm making a note here:
+timeout /t 1 > nul
+echo  HUGE SUCCESS.
+timeout /t 1 > nul
+echo  It's hard to overstate
+timeout /t 1 > nul
+echo  my satisfaction.
+timeout /t 1 > nul
+echo  Aperture Science
+timeout /t 1 > nul
+echo  We do what we must
+timeout /t 1 > nul
+echo  because we can.
+timeout /t 1 > nul
+echo  For the good of all of us.
+timeout /t 1 > nul
+echo  Except the ones who are dead.
+timeout /t 5 > nul
+echo  But there's no sense crying
+timeout /t 1 > nul
+echo  over every mistake.
+timeout /t 1 > nul
+echo  You just keep on trying
+timeout /t 1 > nul
+echo  till you run out of cake.
+timeout /t 1 > nul
+echo  And the Science gets done.
+timeout /t 1 > nul
+echo  And you make a neat gun.
+timeout /t 1 > nul
+echo  For the people who are
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  Forms FORM-55551-5:
+timeout /t 1 > nul
+echo  Personnel File Addendum:
+timeout /t 5 > nul
+echo  Dear {{Subject Name Here}},
+timeout /t 10 > nul
+echo  I'm not even angry.
+timeout /t 1 > nul
+echo  I'm being so sincere right now.
+timeout /t 1 > nul
+echo  Even though you broke my heart.
+timeout /t 1 > nul
+echo  And killed me.
+timeout /t 1 > nul
+echo  And tore me to pieces.
+timeout /t 1 > nul
+echo  And threw every piece into a fire.
+timeout /t 1 > nul
+echo  As they burned it hurt because
+timeout /t 1 > nul
+echo  I was so happy for you!
+timeout /t 1 > nul
+echo  Now these points of data
+timeout /t 1 > nul
+echo  make a beautiful line.
+timeout /t 1 > nul
+echo  And we're out of beta.
+timeout /t 1 > nul
+echo  We're releasing on time.
+timeout /t 1 > nul
+echo  So I'm GLaD. I got burned.
+timeout /t 1 > nul
+echo  Think of all the things we learned
+timeout /t 1 > nul
+echo  for the people who are
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  Forms FORM-55551-6:
+echo  Personnel File Addendum Addendum:
+timeout /t 5 > nul
+echo  One last thing:
+timeout /t 5 > nul
+echo  Go ahead and leave me.
+timeout /t 1 > nul
+echo  I think I prefer to stay inside.
+timeout /t 1 > nul
+echo  Maybe you'll find someone else
+timeout /t 1 > nul
+echo  to help you.
+timeout /t 1 > nul
+echo  Maybe Black Mesa...
+timeout /t 1 > nul
+echo  THAT WAS A JOKE. HA HA. FAT CHANCE.
+timeout /t 1 > nul
+echo  Anyway, this cake is great.
+timeout /t 1 > nul
+echo  It's so delicious and moist.
+timeout /t 1 > nul
+echo  Look at me still talking
+timeout /t 1 > nul
+echo  when there's Science to do.
+timeout /t 1 > nul
+echo  When I look out there,
+timeout /t 1 > nul
+echo  it makes me GLaD I'm not you.
+timeout /t 1 > nul
+echo  I've experiments to run.
+timeout /t 1 > nul
+echo  There is research to be done.
+timeout /t 1 > nul
+echo  On the people who are
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  PS: And believe me I am
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 1 > nul
+echo  PPS: I'm doing Science and I'm
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 1 > nul
+echo  PPPS: I feel FANTASTIC and I'm
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  FINAL THOUGHT:
+timeout /t 1 > nul
+echo  While you're dying I'll be
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  FINAL THOUGHT PS:
+timeout /t 1 > nul
+echo  And when you're dead I will be
+timeout /t 1 > nul
+echo  still alive.
+timeout /t 5 > nul
+echo  STILL ALIVE
+timeout /t 5 > nul
+echo  Still alive.
+pause
+exit
